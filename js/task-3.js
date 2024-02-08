3174b3d16d21774dd0c11efe35d78443e56a9966
wowka130299 <wolodya700@icloud.com>
2024-02-08 17:39:33 +0100

    summary

--- js/task-3.js
+++ js/task-3.js
@@ -1,21 +1,23 @@
-'use strict';
-
-function checkForSpam(message) {
-
-    const messageLow = message.toLowerCase();
-
-    if (messageLow.includes('spam') || messageLow.includes('sale')) {
-        return true;
-    } else { return false; }
-
+function filterArray(numbers, value) {
+    let numbersLength = numbers.length;
+    let result = [];
+    for (let i = 0; i < numbersLength; i++) {
+        if (numbers[i] > value) {
+            result.push(numbers[i]); 
+        }
+        
+    }
+    return result;
+    
 }
 
 
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// Виклик функції filterArray() з випадковим масивом і числом повертає правильний масив
+
+console.log(filterArray([12, 24, 8, 41, 76, 11, 11, 13, 14, 15], 1));
